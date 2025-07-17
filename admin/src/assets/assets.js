d68f7a2fd7268bf2aa28c920f5a5b5d421701c91
pxxja <poojabhambid2004@gmail.com>
2025-07-17 11:53:41 +0530

    add imgs to admin folder

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,13 +4,14 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 import clothsyLogo from './clothsyLogo.png'
-
+import blank_profile from './blank_profile.webp'
 export const assets = {
     logo,
     add_icon,
     order_icon,
     upload_area,
     parcel_icon,
-    clothsyLogo
+    clothsyLogo,
+    blank_profile
 }
 
