7f9372c2b6f756e0ebc098089e81465f1719ccd5
pxxja <poojabhambid2004@gmail.com>
2025-12-11 17:32:39 +0530

    .

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,8 +12,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500 '>54701,wllims station <br/> Suite 305,Mumbai,India</p>
-          <p className='text-gray-500'>Tel: (415) 555-0123 <br/>Email: admin@clothsy.com</p>
+          <p className='text-gray-500 '>54701,Vashi<br/> Sector 17,New Mumbai,India</p>
+          <p className='text-gray-500'>Tel: +91 8935645550 <br/>Email: admin@clothsy.com</p>
           {/* <p className='font-semibold text-xl text-gray-600'>Careers at Clothsy</p>
           <p className='text-gray-500 '>Learn more about our teams and job openings.</p> */}
           {/* <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500' >Explore Jobs</button> */}
